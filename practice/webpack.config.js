874bbd5ec3879901b007ecf1fcd3d134ced39963
hcs1105 <hcs1105@gmail.com>
2023-10-20 20:54:39 +0900

    이미지가 표시되지 않는 현상 수정

--- practice/webpack.config.js
+++ practice/webpack.config.js
@@ -15,6 +15,18 @@ module.exports = {
         test: /\.css$/, // .css 확장자로 끝나는 모든 파일
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+				type: 'asset',
+				generator: {
+					filename: '[name][ext]?[hash]',
+				},   
+				parser: {
+          dataUrlCondition: {
+            maxSize: 5000,
+          },
+        },
+      },
     ]
   }
 }
