1e61eedb27db2d9f10f1788ffaf1ff93f2edfcea
hcs1105 <hcs1105@gmail.com>
2023-10-22 21:07:42 +0900

    preset-env 플러그인을 이용하여 바벨 적용

--- practice/babel.config.js
+++ practice/babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   presets: [
-		'./my-babel-preset.js',
+		// './my-babel-preset.js',
+		['@babel/preset-env', {
+			targets : {
+				chrome: '118', // 크롬 118 버전까지 지원
+				ie: '11', // ie 11까지 지원
+			},
+		}],
 	],
 }
